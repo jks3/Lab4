e59084354257c4101535c3b7c603bf659713d697
jordan <jordan@jordan-Latitude-E6420>
2017-09-26 18:03:58 -0500

    I hope this is more clear...

--- src/main/java/ReclamationProject.java
+++ src/main/java/ReclamationProject.java
@@ -7,20 +7,44 @@
  * 4. Add comments and Javadoc comments where needed
  * 5. Remove unnecessary comments as appropriate
  */
+/**
+ * Checks which substrings of a string is contained in another string
+ * as well as substrings of the second string
+ * Those substrings that are contained within the other string or its
+ * substrings are then returned (includes multiple instances)
+ */
 
 public class ReclamationProject
 {
-    static String doit(String a,String b){
-        if (a.length() > b.length()){
-            String c = a; // TODO: set c to a
-            a=b; b=c;}
-        String r = (a.equals(b)) ? "" : ""; // I love the ternary operator!
-        /*
-         * For loop with i
+    static String whichSubsCanBeFound(final String key, final String within) {
+        String keyToCheckFor = key;
+        String wordToCheckIn = within;
+        /**
+         * Switches values of arguments if the key is longer than the searched string to prevent
+         * out of bounds error
          */
-        for (int i = 0; i < a.length(); i++) { for (int j = a.length() - i; j > 0; j--) {
-                for (int k = 0; k < b.length()- j; k++) {
-                    r = (a.regionMatches(i, b, k, j) && j >r.length()) ? a.substring(i,i + j) : r; // Do it!
-                        }} // Ah yeah
-        } return r; }
+        if (keyToCheckFor.length() > wordToCheckIn.length()){
+            String temp = keyToCheckFor; //Temp to store initial value of key
+            keyToCheckFor = wordToCheckIn ;
+            wordToCheckIn = temp;
+            }
+        String result = ""; //creates empty string to store result
+    /**
+     * The first loop changes the section of the key to be checked
+     * The second and third change the section of within to be checked
+     */
+        for (int begIndOfKey = 0; begIndOfKey < keyToCheckFor.length(); begIndOfKey++) {
+            for (int endIndOfWithin = keyToCheckFor.length() - begIndOfKey;
+                    endIndOfWithin > 0; endIndOfWithin--) {
+                for (int begIndOfWithin = 0;
+                        begIndOfWithin < wordToCheckIn.length()- endIndOfWithin; begIndOfWithin++) {
+                    result = (keyToCheckFor.regionMatches(begIndOfKey, wordToCheckIn,
+                            begIndOfWithin, endIndOfWithin)
+                            && endIndOfWithin >result.length()) ? keyToCheckFor.substring(begIndOfKey,
+                                    begIndOfKey + endIndOfWithin) : result;
+                        }
+                }
+        }
+        return result;
+     }
 }
